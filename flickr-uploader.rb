34b2f952ef37dd2f115cef2dd65af1a375693114
Freenerd <nospam@freenerd.de>
2013-11-18 21:26:54 +0100

    Initial commit

---
+++ flickr-uploader.rb
@@ -0,0 +1,60 @@
+#
+# This script handles the command line input only
+# The actual logic is in /lib
+#
+
+require_relative 'lib/flickr-uploader.rb'
+require 'optparse'
+
+options = {}
+options_parser =
+  OptionParser.new do |opts|
+    opts.banner = "Usage: flickr-uploader.rb [options]"
+
+    opts.on("-c", "--connect", "Interactive mode to connect your Flickr account") do
+      options[:connect] = true
+    end
+
+    opts.on("-u", "--upload DIRECTORY", "Directory of the files to be uploaded to Flickr") do |directory|
+      options[:upload] = true
+      options[:directory] = directory
+    end
+
+    opts.on("-p", "--public", "If set, uploaded items will be public. Otherwise everything will be made private") do
+      options[:public] = true
+    end
+
+    opts.on("-n", "--photoset-name NAME", "Creates a photoset with the given name and adds all uploaed items to it") do |name|
+      options[:photoset] = name
+    end
+
+    opts.on("-d", "--dry", "Don't do the action, but only act as if and show the according output") do
+      options[:dry] = true
+    end
+
+    opts.on("--list-photosets", "List all the photosets") do
+      options[:list_photosets] = true
+    end
+
+    opts.on("--delete-photoset PHOTOSET_ID", "This deletes all photos in a photoset") do |photoset_id|
+      options[:delete_photoset] = photoset_id
+    end
+
+    opts.on_tail("-h", "--help", "Show this message") do
+      puts opts
+      exit
+    end
+  end
+options_parser.parse!
+
+if(options[:connect])
+  FlickrUpload::Config.new.connect
+elsif(options[:upload])
+  FlickrUpload.new(options).upload
+elsif(options[:list_photosets])
+  FlickrUpload.new(options).list_photosets
+elsif(options[:delete_photoset])
+  FlickrUpload.new(options).delete_photoset(options[:delete_photoset])
+else
+  puts options_parser
+end
---
+++ lib/flickr-uploader.rb
@@ -0,0 +1,293 @@
+require 'yaml'
+
+require 'flickraw'
+
+FlickRaw.api_key="15f8ac054e62ed2373cfbd9097730ce3"
+FlickRaw.shared_secret="404cbccb61ecb16a"
+
+class FlickrUpload
+  class Config
+    CONFIG_FILENAME = "config.yml"
+
+    def initialize
+      if File.exists?(CONFIG_FILENAME)
+        file = File.open(CONFIG_FILENAME, "r")
+        @config = YAML.load(file.read)
+
+        unless @config && @config[:access_token] && @config[:access_token_secret]
+          raise "Problem with config.yml. Please delete the file and try to connect again."
+        end
+
+        file.close
+      else
+        @config = {
+          :access_token => nil,
+          :access_token_secret => nil,
+        }
+      end
+    end
+
+    def connect
+      if load_config && username = get_username
+        puts "You already have the Flickr account '#{username}' connected."
+        puts "Do you want to connect a new account? [y/n]"
+        input = gets.chomp
+
+        return if input.match /n/i
+      end
+
+      authenticate
+    end
+
+    def load_config
+      return false unless @config[:access_token] && @config[:access_token_secret]
+
+      flickr.access_token = @config[:access_token]
+      flickr.access_secret = @config[:access_token_secret]
+
+      self
+    end
+
+    def get_username
+      begin
+        return flickr.test.login.username
+      rescue FlickRaw::OAuthClient::FailedResponse
+        return false
+      end
+    end
+
+    private
+
+    def authenticate
+      puts "Authenticating with Flickr ..."
+      token = flickr.get_request_token
+      auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
+
+      puts "Open the following url in your browser and approve the application"
+      puts auth_url
+      puts "Now copy here the number given in the browser:"
+      verify = gets.strip
+
+      begin
+        flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
+        login = flickr.test.login
+        puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
+      rescue FlickRaw::FailedResponse => e
+        puts "Authentication failed : #{e.msg}"
+        return
+      end
+
+      @config[:access_token] = flickr.access_token
+      @config[:access_token_secret] = flickr.access_secret
+
+      write_config
+    end
+
+    def write_config
+      file = File.open(CONFIG_FILENAME, "w")
+      file.puts(YAML.dump(@config))
+      file.close
+    end
+  end
+end
+
+class FlickrUpload
+  class Log
+    LOG_FILENAME = "log.yml"
+
+    def initialize(dry=False)
+      @dry = dry
+
+      if File.exists?(LOG_FILENAME)
+        file = File.open(LOG_FILENAME, "r")
+        @log = YAML.load(file.read)
+        file.close
+      else
+        @log = { :runs => [], :photos => [], :photoset => {} }
+      end
+
+      @log[:runs] << Time.now.to_s
+    end
+
+    def photoset
+      @log[:photoset]
+    end
+
+    def photoset=(dict)
+      @log[:photoset] = dict
+    end
+
+    def add_photo(photo_id, filename)
+      @log[:photos] << { :photo_id => photo_id, :filename => filename }
+    end
+
+    def already_uploaded?(photo_id)
+      @log[:photos].map { |p| p[:filename] }.include?(photo_id)
+    end
+
+    def write
+      unless @dry
+        File.open(LOG_FILENAME, "w") do |file|
+          file.puts(YAML.dump(@log))
+        end
+      end
+    end
+  end
+end
+
+class FlickrUpload
+  def initialize(options)
+    @config = Config.new
+    @config.load_config
+    @config.connect unless @config.get_username
+
+    @options = options
+    puts "Dry Run" if @options[:dry]
+
+    @log = Log.new(@options[:dry])
+  end
+
+  def upload
+    upload_directory(@options[:directory])
+  end
+
+  def list_photosets
+    flickr.photosets.getList(:per_page => 500).each do |photoset|
+      puts "#{photoset.id} #{photoset.title} (#{photoset.photos} photos/ #{photoset.videos} videos)"
+    end
+  end
+
+  def delete_photoset(photoset_id)
+
+    begin
+      photoset = flickr.photosets.getInfo(:photoset_id => photoset_id)
+    rescue FlickRaw::FailedResponse
+      puts "The photoset with the ID #{photoset_id} does not exist."
+      puts "You can see all your photosets by using the --list-photosets option"
+      return
+    end
+
+    puts "You are about to delete photoset #{photoset.id} #{photoset.title} with #{photoset.count_photos} photos and #{photoset.count_videos} videos"
+    puts "Are you sure, you want to delete that photoset including all photos and videos? [y/n]"
+    input = gets.chomp
+    return if input.match /n/i
+
+    counter = {
+      :deleted => 0,
+      :total => photoset.count_photos.to_i + photoset.count_videos.to_i,
+    }
+
+    loop do
+      begin
+        photos_object = flickr.photosets.getPhotos(
+          :photoset_id => photoset_id,
+          :media => 'all',
+          :page => 1,
+          :per_page => 500)
+      rescue FlickRaw::FailedResponse
+        # if a photoset has no items in it, it gets deleted as well
+        # thus we get a FailedResponse and assume we are done
+        break
+      end
+
+      photos_object.photo.each do |item|
+        begin
+          flickr.photos.delete(:photo_id => item.id) unless @options[:dry]
+        rescue
+          puts "Deleting failed ... trying again"
+          sleep 1
+          retry
+        end
+
+        counter[:deleted] += 1
+        puts "[#{counter[:deleted]}/#{counter[:total]}] Deleted photo #{item.id} #{item.title}"
+      end
+    end
+
+    puts "Photoset #{photoset.id} sucessfully deleted"
+  end
+
+  private
+
+  def upload_directory(directory)
+    raise "Can't find directory #{directory}" unless Dir.exists?(directory)
+
+    files = Dir.glob(File.join(directory, "*"))
+    puts "Found #{files.length} files in the directory"
+
+    files.each_with_index do |filename, index|
+      print "[#{index+1}/#{files.length}] "
+
+      if @log.already_uploaded?(filename)
+        puts "File #{filename} has already been uploaded. Skipping."
+      else
+        photo_id = upload_photo(filename)
+        make_private    photo_id unless @options[:public]
+        add_to_photoset photo_id if @log.photoset[:id] || @options[:photoset]
+        @log.add_photo  photo_id, filename
+        @log.write
+      end
+    end
+
+    puts "Finished uploading."
+  end
+
+  def upload_photo(filename)
+    begin
+      photo_id = flickr.upload_photo(filename) unless @options[:dry]
+    rescue => e
+      puts "Photo upload failed with error message #{e}. Trying again in a bit ..."
+      sleep 2
+      retry
+    end
+
+    puts "Uploaded picture #{filename} to id #{photo_id}"
+
+    photo_id
+  end
+
+  def make_private(photo_id)
+    unless @options[:dry]
+      flickr.photos.setPerms(
+        :photo_id => photo_id,
+        :is_public => 0,
+        :is_friend => 0,
+        :is_family => 0,
+        :perm_comment => 3,
+        :perm_addmeta => 3
+      ) unless @options[:dry]
+    end
+
+    photo_id
+  end
+
+  def add_to_photoset(photo_id)
+    unless @options[:dry]
+      if @log.photoset[:id]
+        flickr.photosets.addPhoto(
+          :photoset_id => @log.photoset[:id],
+          :photo_id => photo_id)
+      else
+        photoset = flickr.photosets.create(
+          :title => @options[:photoset],
+          :primary_photo_id => photo_id)
+        @log.photoset = {
+          :id => photoset["id"],
+          :name => @options[:photoset]
+        }
+      end
+    end
+
+    puts "Added photo #{photo_id} to photoset #{@log.photoset[:id]} #{@log.photoset[:name]}"
+
+    photo_id
+  end
+
+  def get_most_recent
+    flickr.photos.recentlyUpdated(:min_date => Time.now.to_i - 60 * 60 * 24, :per_page => 20)
+  end
+
+  def delete_photo(photo_id)
+    flickr.photos.delete(:photo_id => photo_id)
+  end
+end
